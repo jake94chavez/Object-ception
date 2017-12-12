f79d55188a5d45729868bc137662678cb81a28fd
Jake Chavez <jake94chavez@gmail.com>
2017-12-12 10:54:40 -0600

    Activity 2 complete

--- objects2.js
+++ objects2.js
@@ -1,7 +1,7 @@
 const adventurer = {
 	name: "Timothy",
 	hitpoints: 10,
-	belongings: ["sword", "poition", "Tums"]
+	belongings: ["sword", "poition", "Tums"],
 }
 
 //console.log(adventurer.belongings)
@@ -13,9 +13,21 @@ const adventurer = {
 //}
 
 console.log(adventurer.belongings[2]);
-
+// => Tums
 adventurer.belongings.push("Vogue Magazine");
 
 for (let i = 0; i < adventurer.belongings.length; i++) {
 	console.log(adventurer.belongings[i]);
 }
+// => sword, potion, Tums, Vogue Magazine
+
+// Activity 2
+adventurer.companion = { name: "Velma", type: "bat"};
+console.log(adventurer.companion.name);
+// => Velma
+adventurer.companion.name = "Susan";
+console.log(adventurer.companion.name);
+// => Susan
+adventurer.companion2 = { name: "Flik", type: "Insect"};
+console.log(adventurer.companion2.name);
+// => Flik
